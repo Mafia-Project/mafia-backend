75869e0fc0f84824dec34ec0decbc1e0dfe3e6d5
WoojinDeve <jwj060111@gmail.com>
2023-07-24 00:07:53 +0900

    fix :reporter

--- src/main/java/com/poscodx/service/GameEventService.java
+++ src/main/java/com/poscodx/service/GameEventService.java
@@ -1,30 +1,26 @@
 package com.poscodx.service;
 
+import static com.poscodx.domain.GameMessageType.END;
+import static com.poscodx.domain.GameMessageType.NIGHT_END;
+import static com.poscodx.domain.GameMessageType.USER_INFO;
+import static com.poscodx.domain.GameMessageType.VOTE_RESULT;
+import static com.poscodx.utils.MapUtils.toMap;
+import static com.poscodx.utils.SocketTopicUtils.CITIZEN_WIN;
+import static com.poscodx.utils.SocketTopicUtils.MAFIA_WIN;
+import static com.poscodx.utils.SocketTopicUtils.SYSTEM_NAME;
+import static com.poscodx.utils.SocketTopicUtils.getChatTopic;
+import static com.poscodx.utils.SocketTopicUtils.getRoomTopic;
+
 import com.poscodx.domain.ChatType;
 import com.poscodx.domain.Game;
 import com.poscodx.domain.GameMessageType;
 import com.poscodx.domain.GamePlayer;
-import com.poscodx.domain.JobType;
 import com.poscodx.dto.ChatResponse;
 import com.poscodx.dto.GameMassage;
-import com.poscodx.dto.UserInfoResponse;
-import com.poscodx.utils.MapUtils;
-import com.poscodx.utils.SocketTopicUtils;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import static com.poscodx.domain.GameMessageType.*;
-import static com.poscodx.utils.MapUtils.*;
-import static com.poscodx.utils.SocketTopicUtils.*;
-import static com.poscodx.utils.SocketTopicUtils.MAFIA_WIN;
-import static com.poscodx.utils.SocketTopicUtils.SYSTEM_NAME;
-import static com.poscodx.utils.SocketTopicUtils.TIME_REDUCTION_MASSAGE;
-import static com.poscodx.utils.SocketTopicUtils.getRoomTopic;
 
 @Service
 @RequiredArgsConstructor
@@ -51,19 +47,22 @@ public class GameEventService {
     public synchronized void confirmGameEndAfterDeathEvent(Game game, GameMessageType type){
         long citizenNumber = game.getAliveCitizenNumber();
         long mafiaNumber = game.getAliveMafiaNumber();
-        gameInfoService.sendUsers(game.getKey(), USER_INFO);
 
         if (mafiaNumber == 0){
             game.end();
+            gameInfoService.sendUsers(game.getKey(), USER_INFO);
             simpMessagingTemplate.convertAndSend(getRoomTopic(game.getKey()), toMap(new GameMassage(END)));
             messageSent(game.getKey(), toMap(ChatResponse.of(SYSTEM_NAME, CITIZEN_WIN, ChatType.SYSTEM)));
         }else if(mafiaNumber >= citizenNumber){
             game.end();
+            gameInfoService.sendUsers(game.getKey(), USER_INFO);
             simpMessagingTemplate.convertAndSend(getRoomTopic(game.getKey()), toMap(new GameMassage(END)));
             messageSent(game.getKey(), toMap(ChatResponse.of(SYSTEM_NAME, MAFIA_WIN, ChatType.SYSTEM)));
         }else if(type == NIGHT_END){
+            gameInfoService.sendUsers(game.getKey(), USER_INFO);
             messageSent(game.getKey(), toMap(ChatResponse.of(SYSTEM_NAME, "날이 밝았습니다.", ChatType.SYSTEM)));
         }else if(type == VOTE_RESULT){
+            gameInfoService.sendUsers(game.getKey(), USER_INFO);
             messageSent(game.getKey(), toMap(ChatResponse.of(SYSTEM_NAME, "밤이 시작되어 마피아가 활동합니다.", ChatType.SYSTEM)));
         }
     }
